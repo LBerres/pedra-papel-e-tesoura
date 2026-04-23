b91348eaa16f1eeb1a77437bb695188bd69bf6da
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-23 19:12:38 -0300

    Inicio do Projeto Pedra Papel e Tesoura

---
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Pedra, Papel, Tesoura");
+Console.WriteLine("-----------------------------");
+Console.WriteLine("1 - Pedra");
+Console.WriteLine("2 - Papel");
+Console.WriteLine("3 - Tesoura");
+Console.WriteLine("-----------------------------");
+Console.Write("Digite uma opção válida: ");
